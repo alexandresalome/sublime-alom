3b2d4bd95492c58bfb2a3ac301c721e780444251
alexandresalome <alexandre.salome@gmail.com>
2012-03-17 17:32:43 +0100

    Initial commit

---
+++ PhpNamespace.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+'''
+Provides PHP namespace insertion in SublimeText
+
+Config summary (see README.md for details):
+
+    # key binding
+    { "keys": ["ctrl+alt+n"], "command": "php_namespace" }
+
+@author: Alexandre Salomé <alexandre.salome@gmail.com>
+@license: MIT (http://www.opensource.org/licenses/mit-license.php)
+@since: 2012-03-15
+'''
+
+import sublime, sublime_plugin
+
+class PhpNamespaceCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+
+		# Filename to namespace
+		filename = self.view.file_name()
+
+		pos = filename.find("/src/")
+		if (pos == -1):
+			sublime.error_message("No src/ folder in current filepath\n" + filename)
+			return
+
+		if (not filename.endswith(".php")):
+			sublime.error_message("No .php extension")
+			return
+
+		namespace = filename[pos+5:-4].replace("/", "\\")
+
+		sels = self.view.sel()
+		for sel in sels:
+			self.view.erase(edit, sel)
+			self.view.insert(edit, sel.begin(), "namespace " + namespace + ";\n")
---
+++ README.md
@@ -0,0 +1,28 @@
+SublimeText Package by Alexandre Salomé
+=======================================
+
+Installation
+------------
+
+To install the plugin under Linux:
+
+    cd ~/.config/sublime-text-2/Packages
+    git clone https://github.com/Alom/sublime-text-package
+
+This package provides:
+
+* ``php_namespace``: Insert PHP namespace
+
+Shortcuts for commands
+----------------------
+
+You can add it to your shortcuts: go to *Preferences*, *Key Bindings (User)* and
+add the following lines:
+
+    [
+        {
+            "keys": ["ctrl+alt+n"], "command": "php_namespace"
+        }
+    ]
+
+
