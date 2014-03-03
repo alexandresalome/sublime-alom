372190d7855ee833aa1c628efe26f124d0766221
Florian Eckerstorfer <florian@theroadtojoy.at>
2014-03-03 11:48:03 +0100

    Add PSR-4 compatibility.

--- PhpNamespace.py
+++ PhpNamespace.py
@@ -12,7 +12,7 @@ Config summary (see README.md for details):
 @since: 2012-03-15
 '''
 
-import re, sublime, sublime_plugin, os
+import re, sublime, sublime_plugin, os, json
 
 class PhpNamespaceCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -50,6 +50,37 @@ class PhpNamespaceCommand(sublime_plugin.TextCommand):
         else:
             className = filename[pos+len(segment):-4].replace("/", "\\")
             namespace = re.sub(r'\\\w+$', '', className)
+
+            # Check if a composer.json file exists
+            composerFilename = filename[:pos] + os.sep + "composer.json"
+            if os.path.exists(composerFilename):
+                json_data=open(composerFilename)
+                try:
+                    data = json.load(json_data)
+                    json_data.close()
+                    try:
+                        # Iterate through all PSR-4 definitions
+                        for (nsPrefix, nsPath) in data["autoload"]["psr-4"].items():
+                            # Every namespace prefix should end with \\, but let's be tolerate
+                            if not nsPrefix.endswith("\\"):
+                                nsPrefix += "\\"
+                            # If the filename after the breakword starts with the path for the namespace,
+                            # we want to add the namespace prefix to the namespace
+                            if filename[pos:].startswith("/" + nsPath):
+                                # If we are in the "root" (or breakword) directory, the className consists only of the
+                                # className, without a namespace and we want to replace the class name with the
+                                # namespace prefix.
+                                # Else we have a namespace and we add the prefix at the beginning
+                                if className.find("\\") == -1:
+                                    namespace = nsPrefix[:-1]
+                                else:
+                                    namespace = nsPrefix + namespace
+                    except KeyError:
+                        # If there is no autoload or PSR-4 definition in composer.json
+                        pass
+                except ValueError:
+                    # If composer.json is not valid
+                    pass
 
         sels = self.view.sel()
         for sel in sels:
